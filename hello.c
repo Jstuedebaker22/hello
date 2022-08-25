65aab0f383020a19bc67bd509ad58ef276b92c27
Joe Stuedemann <Jstuedemann2@huskers.unl.edu>
2022-08-25 14:09:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Basic Hello World Program
+ * Author:
+ * Date:
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Hello, I'm Joe Stuedemann!\n");
+    printf("Major is Computer Science!\n");
+
+    return 0;
+ }

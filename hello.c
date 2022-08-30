c6e7e704ca927fcadd4dd324d645f3606b80f916
Albina <akysorets2@huskers.uml.edu>
2022-08-30 02:39:21 +0000

    added name and major

--- hello.c
+++ hello.c
@@ -11,6 +11,7 @@
 
     printf("Hello, I'm Joe Stuedemann!\n");
     printf("My Major is Computer Science!\n");
-
+    printf("Albina Kysorets\n");
+    printf("Computer Engineering\n");
     return 0;
  }
